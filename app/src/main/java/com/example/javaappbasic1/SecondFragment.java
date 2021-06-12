f2e0e07a3bee4c8c46740aab76a3f654918aa37a
e <ipbkoa20@gmail.com>
2021-06-12 22:04:33 +0200

    Found TextView for the center of the second fragment and replaced the text in it with a random number

--- app/src/main/java/com/example/javaappbasic1/SecondFragment.java
+++ app/src/main/java/com/example/javaappbasic1/SecondFragment.java
@@ -10,6 +10,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.util.Random;
+
 public class SecondFragment extends Fragment {
     TextView showRandomNumberView;
 
@@ -27,10 +29,19 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Integer count = SecondFragmentArgs.fromBundle(getArguments()).getMyArg();
+        int count = SecondFragmentArgs.fromBundle(getArguments()).getMyArg();
         String countText = getString(R.string.random_heading, count);
         TextView headerView = view.getRootView().findViewById(R.id.textview_header);
         headerView.setText(countText);
+
+        Random random = new Random();
+        int randomNumber = 0;
+        if (count > 0) {
+            randomNumber = random.nextInt(count + 1);
+        }
+
+        TextView centerView = view.getRootView().findViewById(R.id.attributes_random);
+        centerView.setText(getString(R.string.random_number, randomNumber));
 
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
